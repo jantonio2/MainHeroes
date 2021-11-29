062de23d376e4c088290a67985d9f4fffaf12c4a
jantonio2 <jesusrochavedia@gmail.com>
2021-11-29 08:04:47 -0400

    Header hecho

--- src/components/heroes/HeroHeader.js
+++ src/components/heroes/HeroHeader.js
@@ -3,9 +3,9 @@ import React from 'react'
 export const HeroHeader = () => {
   return (
     <div className="header">
-      <img src="" alt="Icon" />
+      <img src="https://i.ibb.co/WPTNVFT/Logo-Marvel.png" alt="Logo-Marvel" border="0" />
       <figure className="title">
-        <img src="" alt="Title" />
+      <img src="https://i.ibb.co/sCZ9KgW/T-tulo.png" alt="TItulo" border="0" />
       </figure>
     </div>
   );
