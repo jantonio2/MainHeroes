a9465918428c7e70de4d958eb06cd92eeed5b8ad
jantonio2 <jesusrochavedia@gmail.com>
2021-11-29 08:42:13 -0400

    Imagenes añadidas

--- src/components/heroes/HeroCard.js
+++ src/components/heroes/HeroCard.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export const HeroCard = () => {
+export const HeroCard = ({id, idMarvel, name, image}) => {
   return (
     <div className="hero-card__container">
-      <img src="https://depor.com/resizer/yWvQcDH97uvQ5xDGBlzb3SD-D2o=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/QRFOZUZC4RHS5GTLM7FHLMEKMA.jpg" alt="HeroImg" />
+      <img src={image} alt="HeroImg" />
     </div>
   );
 };
--- src/helpers/heroes.js
+++ src/helpers/heroes.js
@@ -1,30 +1,36 @@
 export const heroes = [{
   id: 107,
   idMarvel: 1017109,
-  name: "Black Widow" 
+  name: "Black Widow",
+  image: "https://i.ibb.co/LPKn3Xp/Black-Widow.png" 
 },
 {
   id: 149,
   idMarvel: 1009220,
-  name: "Captain America"
+  name: "Captain America",
+  image: "https://i.ibb.co/cNpFFhn/Hulk.png" 
 },
 {
   id: 313,
   idMarvel: 1017108,
-  name: "Hawk eye"
+  name: "Hawk eye",
+  image: "https://i.ibb.co/JvyjZ4m/hawkeye.png" 
 },
 {
   id: 332,
   idMarvel: 1009351,
-  name: "Hulk"
+  name: "Hulk",
+  image: "https://i.ibb.co/cNpFFhn/Hulk.png" 
 },
 {
   id: 346,
   idMarvel: 1009368,
-  name: "Iron man"
+  name: "Iron man",
+  image: "https://i.ibb.co/cNpFFhn/Hulk.png" 
 },
 {
   id: 659,
   idMarvel: 1009664,
-  name: "Thor"
+  name: "Thor",
+  image: "https://i.ibb.co/cNpFFhn/Hulk.png"
 }];
